7490fbcb20dbfba20b1d38dae1be66f9e5614029
DESKTOP-JO3QE3M\82108 <leejin914418@gmail.com>
2022-12-10 21:35:25 +0900

    feat: 메인 페이지 디테일 제외 구현, 네비게이션바 버튼 구현

--- mainpage/script.js
+++ mainpage/script.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
   $(document).snowfall({deviceorientation : true, round : true, minSize: 1, maxSize:8,  flakeCount : 250});
 
+  $("#ComposeBtn").click(function(){
+    $(".composeWindow").css("display","block");
+    $(".inboxWindow").css("display","none");
+    $(".outboxWindow").css("display","none");
+  })
+
+  $("#InboxBtn").click(function(){
+    $(".composeWindow").css("display","none");
+    $(".inboxWindow").css("display","block");
+    $(".outboxWindow").css("display","none");
+  })
+
+  $("#OutboxBtn").click(function(){
+    $(".composeWindow").css("display","none");
+    $(".inboxWindow").css("display","none");
+    $(".outboxWindow").css("display","block");
+  })
+
+
 })
