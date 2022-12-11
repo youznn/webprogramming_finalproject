4cf6a6134398022faf939aae42779db9e75d8bda
DESKTOP-JO3QE3M\82108 <leejin914418@gmail.com>
2022-12-11 16:31:47 +0900

    feat: 패스워드 해결

--- loginpage/script.js
+++ loginpage/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
   let checkpassword =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,}/;
   let checkId = /^[a-zA-z0-9]{4,10}$/
   //0 if each item does not meet the condition, 1 if it does
-  let checkall=[0,0,0];
+  let checkall=[0,0,0,0];
 
   //store gender value
 
@@ -131,7 +131,7 @@ $("#subSignUpBtn").click(function(){
     $("#password").css("background-color","#F8E0E0");
     $("#pw_warn").css("color","red");
   }
-  if(checkall[3] == 0 || !$("confirmpassword").val()){ //password is invalid
+  if(checkall[3] == 0){ //password is invalid
     $("#confirmpassword").css("background-color","#F8E0E0");
     $("#cpw_warn").css("color","red");
   }
